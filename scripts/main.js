fef416d688b7b93fe9d61f013f53c3d59fd7e03b
Nakedi <naks702@gmail.com>
2025-10-02 13:22:24 +0200

    working on Javascript form validation

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,29 @@
 
-console.log(document.forms["sponsorForm"]["firstName"])
+
+
 
 function formValidation(){
-let form = document.forms["sponsorForm"]["firstName"];
+let firstName = document.forms["sponsorForm"]["firstName"].value;
+let lastName = document.forms["sponsorForm"]["lastName"].value;
 
-let firstName = document.getElementById("firstName");
-let lastName =document.getElementById("splastName");
-let name = document.getElementById("spFirstName");
-let email = document.getElementById("spEmail");
-let cellphone = document.getElementById("Spcellphone");
+
+// let cellphone = document.forms["sponsorForm"]["cellphone"].value;
+// let email = document.forms["sponsorForm"]["email"].value;
+if(firstName == ""){
+    document.getElementById("validateFirstName").innerHTML ="Enter first name";
+    return false;
+}
+if(lastName == ""){
+   document.getElementById("validatelastName").innerHTML = "Enter last name";
+   return false;
+    
+}
 
 
 }
 
 
 //testing if java is working
-console.log("hello")
-console.log("hello budd")
+
 
 
