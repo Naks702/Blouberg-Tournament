1901e719fd66ef540a9ee92768bfb7879aae11c4
sugar <naks702@gmail.com>
2025-10-06 22:26:13 +0200

    form validation working ok

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,10 @@
 function formValidation(){
     isValid = true;
     let firstname =  document.getElementById("firstName").value;
-    // let lastName =  document.getElementById("lastName").value;
-    // let cellphone =  document.getElementById("cellphone").value;
-    // let email   =  document.getElementById("email").value;
-    let opt;
-  switch(opt){
-   case firstname.value;
-  }
-    
+    let lastName =  document.getElementById("lastName").value;
+    let cellphone =  document.getElementById("cellphone").value;
+    let email   =  document.getElementById("email").value;
+  
     if(firstname ==""){
       document.getElementById("validateFirstName").innerHTML = "enter first name"   
         isValid = false;
@@ -18,14 +14,15 @@ function formValidation(){
        document.getElementById("validateLastName").innerHTML ="enter the last name";
         isValid = false;
     }
-    //   if(cellphone == ""){
-    //   document.getElementById("validateCellphone").innerHTML ="enter cellphone";
-    //     isValid = false;
-    // }
-    //   if(email == ""){
-    //    document.getElementById("validateEmail").innerHTML ="enter email";
-    //     isValid = false;
-    // }
+      else if(cellphone == "" || cellphone.length !=10){
+      document.getElementById("validateCellphone").innerHTML ="enter  valid cellphone";
+        isValid = false;
+    }
+      else if(email == "" || !email.includes("@") || !email.includes(".com") || email.indexOf("@") ==0 || !email.includes(".za") ){
+
+       document.getElementById("validateEmail").innerHTML ="enter valid email";
+        isValid = false;
+    }
     else
     {
         
@@ -34,37 +31,6 @@ function formValidation(){
     return isValid;
 }
 
-// var form = document.getElementById("sponsorForm");
-// async function formSubmit(params) {
-//     params.preventDefault();
-//     var status = document.getElementById("validateFirstName")
-//     var data = new FormData(Event.target);
-//     fetch(event.target.action, {
-//         method: form.method,
-//         body: data,
-//         headers:{
-//             'Accept' : 'application/json'
-//         }
-//     }).then(Response =>{
-//         if(Response.ok){
-//             status.innerHTML ="Thank you for your submission, member of our staff will be in touch";
-//         form.reset()  
-//         }else{
-//             Response.json().then(data =>{
-//                 if(Object.hasOwn(data, "errors")){
-//                  status.innerHTML = data['errors'].map(errors =>errors["message"].join(","))
-//                 }else{
-//                 status.innerHTML ="error submiting the form"
-//                 }
-//             })
-//         }
-      
-
-//     }).catch(errors =>{
-//         status.innerHTML ="error submiting the form"
-//     })
-// }
-// form.addEventListener('submit', formSubmit)
 
 
 
