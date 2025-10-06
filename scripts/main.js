1b2adaa3fc703882d2d7cab71ad8a518c7a07009
Nakedi <naks702@gmail.com>
2025-10-06 16:55:09 +0200

    Working on JS form validation

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,68 @@
 
 function formValidation(){
-let firstName = document.forms["sponsorForm"]["firstName"].value;
-// let lastName = document.forms["sponsorForm"]["lastName"].value;
-// let inValid = false;
+    isValid = true;
+    let firstname =  document.getElementById("firstName").value;
+    let lastName =  document.getElementById("lastName").value;
+    let cellphone =  document.getElementById("cellphone").value;
+    let email   =  document.getElementById("email").value;
 
-// let cellphone = document.forms["sponsorForm"]["cellphone"].value;
-// let email = document.forms["sponsorForm"]["email"].value;
-if(firstName == ""){
-    document.getElementById("validateFirstName").innerHTML ="Enter first name";
-   return false;
-}else{
-    return true;
+    let validateFirstName = "enter the first name";
+    if(firstname ==""){
+       validateFirstName;
+        firstname.style.display ="block"
+        isValid = false;
+    }
+    // if(lastName == ""){
+    //    document.getElementById("validateLastName").innerHTML ="enter the last name";
+    //     isValid = false;
+    // }
+    //   if(cellphone == ""){
+    //   document.getElementById("validateCellphone").innerHTML ="enter cellphone";
+    //     isValid = false;
+    // }
+    //   if(email == ""){
+    //    document.getElementById("validateEmail").innerHTML ="enter email";
+    //     isValid = false;
+    // }
+    else
+    {
+        
+    }
+
+    return isValid;
 }
-// if(lastName == ""){
-//    document.getElementById("validatelastName").innerHTML = "Enter last name";
-//   return false;
-    
+
+// var form = document.getElementById("sponsorForm");
+// async function formSubmit(params) {
+//     params.preventDefault();
+//     var status = document.getElementById("validateFirstName")
+//     var data = new FormData(Event.target);
+//     fetch(event.target.action, {
+//         method: form.method,
+//         body: data,
+//         headers:{
+//             'Accept' : 'application/json'
+//         }
+//     }).then(Response =>{
+//         if(Response.ok){
+//             status.innerHTML ="Thank you for your submission, member of our staff will be in touch";
+//         form.reset()  
+//         }else{
+//             Response.json().then(data =>{
+//                 if(Object.hasOwn(data, "errors")){
+//                  status.innerHTML = data['errors'].map(errors =>errors["message"].join(","))
+//                 }else{
+//                 status.innerHTML ="error submiting the form"
+//                 }
+//             })
+//         }
+      
+
+//     }).catch(errors =>{
+//         status.innerHTML ="error submiting the form"
+//     })
 // }
-
-
-}
-
-
-//testing if java is working
+// form.addEventListener('submit', formSubmit)
 
 
 
